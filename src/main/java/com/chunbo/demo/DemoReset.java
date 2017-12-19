7a7f3656e0ff4fb22a38950b707a029fda2343a4
CHUNBO\zhangzhaoyuan <zhangzhaoyuan@chunbo.com>
2017-12-19 18:42:14 +0800

    测试reset

---
+++ src/main/java/com/chunbo/demo/DemoReset.java
@@ -0,0 +1,15 @@
+package com.chunbo.demo;
+
+/**
+ * Created with IntelliJ IDEA Ultimate.
+ *
+ * @Author zhangzhaoyuan
+ * @Description
+ * @Datetime 2017-12-19 18:41
+ */
+public class DemoReset {
+
+    public static void main(String[] args) {
+        System.out.println("demo");
+    }
+}
