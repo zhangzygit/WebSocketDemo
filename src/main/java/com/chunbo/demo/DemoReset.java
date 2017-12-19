f6f8536adabb49dd1920ffcefc6a1e364e37a4ba
CHUNBO\zhangzhaoyuan <zhangzhaoyuan@chunbo.com>
2017-12-19 19:12:20 +0800

    Revert "2/0"
    This reverts commit 1b53b5d

--- src/main/java/com/chunbo/demo/DemoReset.java
+++ src/main/java/com/chunbo/demo/DemoReset.java
@@ -12,8 +12,5 @@ public class DemoReset {
     public static void main(String[] args) {
         System.out.println("demo");
         System.out.println("1/0");
-        System.out.println(2/0);
-        System.out.println(3/0);
-
     }
 }
